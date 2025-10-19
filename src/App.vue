ccfea8bd6caacc113a27eb1a3073ae99e0b906ab
Stefan Malcek <stefan.malcek1@gmail.com>
2025-10-19 11:38:52 +0200

    refactor: Extracts movie form to dedicated component
    Moves the movie form logic and template into a separate component for better organization and reusability.
    This change enhances code maintainability and simplifies the main application component.

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,17 @@ import { computed, reactive, ref } from "vue";
 import { cloneDeep } from "../utils/clone.js";
 import { normalizeRating } from "../utils/number.js";
 import MovieItem from "../components/MovieItem.vue";
+import MovieForm from "../components/MovieForm.vue";
 
 const movies = ref(cloneDeep(items));
+const selectedMovie = reactive({
+  id: null,
+  name: null,
+  description: null,
+  image: null,
+  genres: [],
+  inTheaters: false,
+});
 
 const averageRating = computed(() => {
   const filledRatings = movies.value.filter((m) => m.rating);
@@ -32,26 +41,11 @@ const resetRatings = () => {
   });
 };
 
-const showMovieForm = ref(false);
+const showMovieDialog = ref(false);
 
 const showForm = () => {
-  showMovieForm.value = true;
+  showMovieDialog.value = true;
 };
-
-const availableGenres = reactive(["Action", "Comedy", "Drama", "Sci-Fi"]);
-const form = reactive({
-  id: null,
-  name: null,
-  description: null,
-  image: null,
-  genres: [],
-  inTheaters: false,
-});
-
-const errors = reactive({
-  name: null,
-  image: null,
-});
 
 const removeMovie = (movieId) => {
   movies.value = movies.value.filter((movie) => movie.id !== movieId);
@@ -60,99 +54,61 @@ const removeMovie = (movieId) => {
 const editMovie = (movieId) => {
   const movie = movies.value.find((movie) => movie.id === movieId);
 
-  form.id = movie.id;
-  form.name = movie.name;
-  form.description = movie.description;
-  form.image = movie.image;
-  form.genres = movie.genres;
-  form.inTheaters = movie.inTheaters;
+  selectedMovie.id = movie.id;
+  selectedMovie.name = movie.name;
+  selectedMovie.description = movie.description;
+  selectedMovie.image = movie.image;
+  selectedMovie.genres = movie.genres;
+  selectedMovie.inTheaters = movie.inTheaters;
 
   showForm();
 };
 
-const hideForm = () => {
-  showMovieForm.value = false;
-  resetForm();
+const hideDialog = () => {
+  showMovieDialog.value = false;
 };
 
-const saveMovie = () => {
-  if (!validate(form)) {
-    return;
-  }
-
-  if (form.id) {
-    updateMovie();
+const saveMovie = (movieForm) => {
+  if (movieForm.id) {
+    updateMovie(movieForm);
   } else {
-    addMovie();
+    addMovie(movieForm);
   }
 
-  hideForm();
+  hideDialog();
 };
 
-const addMovie = () => {
-  if (!validate(form)) {
+const addMovie = (movieForm) => {
+  if (!movieForm) {
     return;
   }
 
   const movie = {
     id: Number(Date.now()),
-    name: form.name,
-    description: form.description,
-    image: form.image,
-    genres: form.genres,
-    inTheaters: form.inTheaters,
+    name: movieForm.name,
+    description: movieForm.description,
+    image: movieForm.image,
+    genres: movieForm.genres,
+    inTheaters: movieForm.inTheaters,
   };
   movies.value.push(movie);
 };
 
-const updateMovie = () => {
-  if (!validate(form)) {
+const updateMovie = (movieForm) => {
+  if (!movieForm) {
     return;
   }
 
-  const movie = movies.value.find((m) => m.id === form.id);
+  const movie = movies.value.find((m) => m.id === movieForm.id);
   if (!movie) {
     return;
   }
 
-  movie.name = form.name;
-  movie.description = form.description;
-  movie.image = form.image;
-  movie.genres = form.genres;
-  movie.inTheaters = form.inTheaters;
-};
-
-const validate = (input) => {
-  clearErrors();
-  let success = true;
-
-  if (!input.name) {
-    errors.name = "Name is required";
-    success = false;
-  }
-
-  if (!input.image) {
-    errors.image = "Image is required";
-    success = false;
-  }
-
-  return success;
-};
-
-const resetForm = () => {
-  form.id = null;
-  form.name = null;
-  form.description = null;
-  form.image = null;
-  form.genres = [];
-  form.inTheaters = false;
-
-  clearErrors();
-};
-
-const clearErrors = () => {
-  errors.name = null;
-  errors.image = null;
+  movie.name = movieForm.name;
+  movie.description = movieForm.description;
+  movie.image = movieForm.image;
+  movie.genres = movieForm.genres;
+  movie.inTheaters = movieForm.inTheaters;
 };
 </script>
 
@@ -161,72 +117,14 @@ const clearErrors = () => {
     class="max-w-full h-screen flex flex-col items-center justify-center mx-auto"
   >
     <div
-      v-if="showMovieForm"
+      v-if="showMovieDialog"
       class="absolute inset-0 backdrop-blur bg-gray-800/40 z-10 flex items-center justify-center"
     >
-      <form
-        @submit.prevent="saveMovie"
-        class="shrink-0 w-full max-w-2xl rounded-md flex flex-col shadow-2xl bg-gray-800 p-4 space-y-5 text-white"
-      >
-        <div class="form-element">
-          <label for="name">Name</label>
-          <input id="name" type="text" v-model="form.name" class="form-input" />
-          <span v-if="errors.name" class="text-red-500">{{ errors.name }}</span>
-        </div>
-        <div class="form-element">
-          <label for="description">Description</label>
-          <textarea
-            id="description"
-            v-model="form.description"
-            class="form-input"
-          />
-        </div>
-        <div class="form-element">
-          <label for="image">Image</label>
-          <input
-            id="image"
-            type="text"
-            v-model="form.image"
-            class="form-input"
-          />
-          <span v-if="errors.image" class="text-red-500">{{
-            errors.image
-          }}</span>
-        </div>
-        <div class="form-element">
-          <label for="genres">Genres</label>
-          <select id="genres" multiple v-model="form.genres" class="form-input">
-            <option
-              v-for="genre in availableGenres"
-              :key="genre"
-              :value="genre"
-            >
-              {{ genre }}
-            </option>
-          </select>
-        </div>
-        <div class="form-element">
-          <label class="flex items-center justify-start space-x-2">
-            <input id="inTheaters" type="checkbox" v-model="form.inTheaters" />
-            <span>In Theaters</span>
-          </label>
-        </div>
-        <div class="flex items-center justify-between w-full">
-          <button
-            type="button"
-            class="text-white bg-gray-500 rounded px-4 py-2"
-            @click="hideForm"
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            class="text-white bg-blue-400 rounded px-4 py-2"
-          >
-            Save
-          </button>
-        </div>
-      </form>
+      <MovieForm
+        :model-value="selectedMovie"
+        @update:modelValue="saveMovie"
+        @cancel="hideDialog"
+      />
     </div>
     <div class="w-full flex items-center justify-between max-w-6xl p-4 mb-4">
       <div class="flex items-center justify-center gap-4 text-white">
@@ -262,16 +160,4 @@ const clearErrors = () => {
   </div>
 </template>
 
-<style>
-.form-element {
-  @apply flex flex-col items-start justify-start w-full;
-}
-
-.form-input {
-  @apply w-full rounded-md bg-gray-900  border border-white/50 focus:border-blue-600 focus:outline-none px-2 py-1;
-}
-
-.float-button {
-  @apply rounded-full bg-gray-200 p-3 hover:text-white;
-}
-</style>
+<style></style>
